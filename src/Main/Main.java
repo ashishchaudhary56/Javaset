cb64778b2ad4ee49086c60ba17cb9a317edbe6bd
Ashish Chaudhary <ashish.chaudhary_cs18@gla.ac.in>
2019-08-23 06:31:44 +0530

    Initial commit

---
+++ src/Main/Main.java
@@ -0,0 +1,21 @@
+package Main;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner my= new Scanner(System.in);
+        System.out.println("enter two numbers");
+        int n1,n2,sum;
+        n1=my.nextInt();
+        n2=my.nextInt();
+        sum=n1+n2;
+        System.out.println("sum of two numbers is"+sum);
+    }
+
+
+
+
+
+}
